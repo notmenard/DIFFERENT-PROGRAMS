4f98c19e1716a62b7c20aee26dd8941976fb642c
Menard <menardsabugo@gmail.com>
2024-03-23 21:37:48 +0800

    Defining Function

--- FancyWayOfPrinting.py
+++ FancyWayOfPrinting.py
@@ -4,6 +4,12 @@ import time
 from colorama import Fore
 
 # Defining function to print text with animation effect
+def print_with_animation(text, color=Fore.WHITE, delay=0.03):
+    for char in text:
+        print (color + char, end='', flush=True)
+        time.sleep(delay) # Adding Delay
+    print()
+
 # Defining function to print ASCII art with animation effect
 # Taking user's input for the name and their dream job
 # Printing the ASCII art for the user's name with specified color and width
