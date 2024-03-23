1c5e529d42bfad0c0ebd1c1f3d5a13f6f39b6747
Menard <menardsabugo@gmail.com>
2024-03-23 21:31:04 +0800

    Pseudocode

---
+++ FancyWayOfPrinting.py
@@ -0,0 +1,6 @@
+# Importing different libraries for different functions to be used in this program
+# Defining function to print text with animation effect
+# Defining function to print ASCII art with animation effect
+# Taking user's input for the name and their dream job
+# Printing the ASCII art for the user's name with specified color and width
+# Printing the ASCII art for the user's dream job with specified color and width
