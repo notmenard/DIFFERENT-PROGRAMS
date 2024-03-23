9ee4c1af036405ffb47ac5d0cc9e81d68da18b29
Menard <menardsabugo@gmail.com>
2024-03-23 21:59:31 +0800

    Finalizing Code

--- FancyWayOfPrinting.py
+++ FancyWayOfPrinting.py
@@ -21,12 +21,12 @@ def print_ascii_art(text, font='isometric1', color=Fore.YELLOW, width=150, delay
 
 # Assigning Variable and Getting user's input for each questions
 name = input("Enter your name: ")
-birthday = input("When is your birthday? ")
+birthdate = input("When is your birthdate? ")
 dream_job = input("What is your dream job? ")
 school_attending = input("Which school are you currently attending? ")
 
 # Printing the ASCII art for the user's data with specified color and width
 print_ascii_art(name, color=Fore.CYAN, width=200, delay=0.0001)
-print_ascii_art(birthday, color=Fore.MAGENTA, width=200, delay=0.0001)
+print_ascii_art(birthdate, color=Fore.MAGENTA, width=200, delay=0.0001)
 print_ascii_art(dream_job, color=Fore.GREEN, width=240, delay=0.0001)
-print_ascii_art(school_attending, color=Fore.LIGHTMAGENTA_EX, width=240, delay=0.0001)
+print_ascii_art(school_attending, color=Fore.BLUE, width=240, delay=0.0001)
