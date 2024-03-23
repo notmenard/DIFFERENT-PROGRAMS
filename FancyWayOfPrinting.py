ee7d6d0bac0bf1df9b748fd586a5a8506aa9cf33
Menard <menardsabugo@gmail.com>
2024-03-23 21:58:22 +0800

    Printing the ASCII art for user's data

--- FancyWayOfPrinting.py
+++ FancyWayOfPrinting.py
@@ -3,12 +3,14 @@ import pyfiglet
 import time
 from colorama import Fore
 
+
 # Defining function to print text with animation effect
 def print_with_animation(text, color=Fore.WHITE, delay=0.03):
     for char in text:
         print(color + char, end='', flush=True)
         time.sleep(delay)  # Adding Delay
     print()
+
 
 # Defining function to print ASCII art with animation effect
 def print_ascii_art(text, font='isometric1', color=Fore.YELLOW, width=150, delay=0.001):
@@ -25,3 +27,6 @@ school_attending = input("Which school are you currently attending? ")
 
 # Printing the ASCII art for the user's data with specified color and width
 print_ascii_art(name, color=Fore.CYAN, width=200, delay=0.0001)
+print_ascii_art(birthday, color=Fore.MAGENTA, width=200, delay=0.0001)
+print_ascii_art(dream_job, color=Fore.GREEN, width=240, delay=0.0001)
+print_ascii_art(school_attending, color=Fore.LIGHTMAGENTA_EX, width=240, delay=0.0001)
